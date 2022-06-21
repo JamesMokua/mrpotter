cc1efa3efeaaa58c675c4218336bb0ff5c1373a4
James Mokua <jamieedgar02@gmail.com>
2022-06-21 22:42:59 +0300

    Elixir bugs fixed

--- src/components/Elixirs.js
+++ src/components/Elixirs.js
@@ -1,31 +1,72 @@
 import React,{useState,useEffect} from 'react'
-import { v4 as uuidv4 } from 'uuid';
-import ElixirDisplay from './ElixirDisplay';
+
 
 const Elixirs = () => {
-  //fetch data from API
-    const [elixirs, setElixirs] = useState([])
-    const fetchQuote = async() =>{
-        return await fetch('https://wizard-world-api.herokuapp.com/Elixirs')
-        .then(response => response.json());
+  const[elixirs,setElixirs] = useState(null);
+  const[loading,setLoading] = useState(true)
+  const[error,setError] = useState(null)
+  // fetch data using async await
+  const getData =  async() => {
+    //fetches data and sets it to the state
+    try{
+      const response = await fetch('https://wizard-world-api.herokuapp.com/Elixirs');
+      if(!response.ok){
+        throw new Error(
+          `This is an HTTP Error: The status is ${response.status}`
+        );
       }
-       
-    useEffect(() => {
-      async function fetchData(){
-        setElixirs(await fetchQuote());
-      }
-     fetchData();
-     
-    }, [])
-      const elixirList =  elixirs.map(inner => [inner].map((elixirs) =>
-      <ElixirDisplay key={uuidv4()} elixirs={elixirs}/>
-      
-      ));
+      let elixirs = await response.json();
+      setElixirs(elixirs);
+      setError(null);
+    } catch(err){
+      //catches error in case of network failure
+      setError(err.message);
+      setElixirs(null);
+    } finally {
+      //sets loading to false and displays fetched data
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+   getData()
+  },[])
   
   return (
     <div>
     <div>Elixirs</div>
-    <div>{(elixirList.length >= 1 ? <div>{elixirList}</div>: <div>Loading....</div>)}</div>
+    {/*manage loading and display error message*/}
+      {loading && <div>A moment please...</div>}
+      {error && (
+        <div> { `There is a problem fetching the post data - ${error}`}</div>
+      )}
+      <ul>
+      {/*map through the fetched data,destructure it and display it*/}
+      {/*if else to show not available if data not included in fetched data*/}
+        {
+          elixirs &&
+          elixirs.map(({id,name,effect,sideEffects,characteristics,time,difficulty,ingredients,inventors,manufacturer}) => (
+            <li key={id}>
+              <div><b>Name:</b>{(name != null ? <div> {name} </div> : <div>Not Available</div> )}</div> 
+        <div><b>Effect:</b>{(effect != null ? <div> {effect} </div> : <div>Not Available</div> )}</div> 
+        <div><b>Side Effect:</b>{(sideEffects != null ? <div> {sideEffects} </div> : <div>Not Available</div> )}</div>  
+        <div><b>Characteristics:</b>{(characteristics != null ? <div> {characteristics} </div> : <div>Not Available</div> )}</div>    
+        <div><b>Time:</b>{(time != null ? <div> {time} </div> : <div>Not Available</div> )}</div>   
+        <div><b>Difficulty:</b>{(difficulty != null ? <div> {difficulty} </div> : <div>Not Available</div> )} </div>           
+        {/*map through ingredients and inventors and display data inside*/}
+        <div><b>Ingredients:</b> {(ingredients.length >= 1 ? <>{ingredients.map(({name}) => (
+        <ul>
+          <li>{name}</li>
+        </ul>
+        ))}</>: <div>Not Available</div>)}</div> 
+        <div><b>Inventors:</b> {(inventors.length >= 1 ? <>{inventors.map(({firstName,lastName}) => (
+          <div>{firstName +  lastName}</div>
+        ))}</>: <div>Not Available</div>)}</div> 
+        <div><b>Manufacturers: </b>{(manufacturer != null ? <div> {manufacturer} </div> : <div>Not Available</div> )}</div> 
+              <hr/>
+            </li>
+          ))
+        }
+      </ul>
     </div>
 
   )
